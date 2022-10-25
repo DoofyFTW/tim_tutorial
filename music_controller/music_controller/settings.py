adf2e4f5afd2530e63153dc6a03f143e76b4393c
Daniel Dufek <105704@vfn.cz>
2022-10-25 11:19:57 +0200

    git secret fix

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -1,3 +1,5 @@
+import environ
+
 """
 Django settings for music_controller project.
 
@@ -12,6 +14,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+environ.Env.read_env(BASE_DIR / ".env")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7j!##hj#^&j*^zlgpvvac2v3gm0$h6f9dt%i!79t6nlp-^@ezr"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
