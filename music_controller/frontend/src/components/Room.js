816b04a7cee9626dfd2072df206093e7f87189c2
Daniel Dufek <105704@vfn.cz>
2022-10-27 16:17:46 +0200

    10 6min

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Grid, Button, Typography} from '@material-ui/core'
+import { Link } from 'react-router'
 
 export default class Room extends Component {
     constructor(props) {
@@ -9,6 +11,7 @@ export default class Room extends Component {
             isHost: false,
         };
         this.roomCode = this.props.match.params.roomCode;
+        this.leaveButtonPressed = this.leaveButtonPressed.bind(this)
         this.getRoomDetails()
     }
 
@@ -25,14 +28,41 @@ export default class Room extends Component {
             });
     }
 
+    leaveButtonPressed() {
+        
+
+    }
+
     render() {
         return (
-        <div>
-            <h3>{this.roomCode}</h3>
-            <p>Votes: {this.state.votesToSkip}</p>
-            <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-            <p>Host: {this.state.isHost.toString()}</p>
-        </div>
+        <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography variant="h4" component="h4">
+                    Code: {this.roomCode}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Typography variant="h6" component="h6">
+                    Votes: {this.state.votesToSkip}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Typography variant="h6" component="h6">
+                Guest Can Pause: {this.state.guestCanPause.toString()}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Typography variant="h6" component="h6">
+                Host: {this.state.isHost.toString()}
+                </Typography>
+
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button variant="contained" color="secondary" to="/" component={Link}>
+                    Leave Room
+                </Button>
+            </Grid>
+        </Grid>
         );
     }
 
